31252cf768824e2cb3fb33a9c46238b7e897f4ee
Vincenzo Tarricone <vinz486@gmail.com>
2026-07-19 12:13:21 +0200

    Refresh cache for new sound effects

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "luci-magiche-v5";
-const OFFLINE_PAGE = "./?app-version=5";
+const CACHE_NAME = "luci-magiche-v6";
+const OFFLINE_PAGE = "./?app-version=6";
 const APP_FILES = [
   OFFLINE_PAGE,
   "./manifest.webmanifest",

42f7adf628a68618e632d1c7d6256903f530215f
Vincenzo Tarricone <vinz486@gmail.com>
2026-07-19 11:44:21 +0200

    Refresh Luci Magiche cache for drag reordering

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "luci-magiche-v2";
-const OFFLINE_PAGE = "./?app-version=2";
+const CACHE_NAME = "luci-magiche-v3";
+const OFFLINE_PAGE = "./?app-version=3";
 const APP_FILES = [
   OFFLINE_PAGE,
   "./manifest.webmanifest",
